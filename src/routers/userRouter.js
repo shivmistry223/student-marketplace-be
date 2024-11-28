43be41957d91694d5251bdccab2d88139bcc1e9f
Shiv Mistry <shivmistry223@gmail.com>
2024-11-27 21:10:07 -0500

    integrated update user

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -46,4 +46,47 @@ router.get("/user/:id", async (req, res) => {
   }
 });
 
+router.put("/user", async (req, res) => {
+  const allowableUpdates = [
+    "firstName",
+    "lastName",
+    "courseCode",
+    "termNo",
+    "phoneNumber",
+  ];
+
+  try {
+    const user = await User.findById(req.body.id);
+    allowableUpdates.forEach((key) => (user[key] = req.body[key]));
+    await user.save();
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ error: error.message });
+  }
+});
+
+router.post("/reset-password", async (req, res) => {
+  const { id, newPassword, confirmPassword } = req.body;
+
+  if (newPassword !== confirmPassword) {
+    return res.status(400).send({ error: "Passwords do not match!" });
+  }
+
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).send({ error: "User not found!" });
+    }
+
+    // Hash the new password
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+
+    await user.save();
+    res.send({ message: "Password reset successful!" });
+  } catch (err) {
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+});
 module.exports = router;
